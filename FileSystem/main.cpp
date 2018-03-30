567ee962c10e106c7089415d0d57bf60acc281d2
NotAPenguin <michiel.vandeginste@gmail.com>
2018-03-30 16:32:56 +0200

    starting work again

--- FileSystem/main.cpp
+++ FileSystem/main.cpp
@@ -3,7 +3,28 @@
 #include <thread>
 #include <cstdint>
 #include <vector>
+#include <string>
+#include <queue>
+#include <sstream>
 
+struct AsyncRequest
+{
+	std::string file;
+
+	//will contain all data when reading is complete
+	std::stringstream data;
+};
+
+class Async
+{
+public:
+	void wait_for_request(AsyncRequest r);
+	AsyncRequest add_request(std::string path);
+
+private:
+	std::thread m_read_thread;
+	std::queue<AsyncRequest> requests;
+};
 
 int main()
 {
